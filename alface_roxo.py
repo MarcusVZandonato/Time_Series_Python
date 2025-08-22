e053a5a2574208403d524580ca6f2c160db4d4ef
MarcusVZandonato <mviniciuszand31@gmail.com>
2025-08-21 21:29:01 -0300

    feature: script padrão

---
+++ alface_roxo.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+@Autor: marcus vinicius zandonato
+@Data: 21/08/2025
+@Versão: 1.0
+@Descrição:
+    Este script serve para dar as boas-vindas ao usuário e mostrar a estrutura básica
+    de um arquivo Python padrão.
+"""
+#-----------------------------------------------------------------------------
+# Imports
+#-----------------------------------------------------------------------------
+import sys
+import os
+# ... (o resto do código é o mesmo)
